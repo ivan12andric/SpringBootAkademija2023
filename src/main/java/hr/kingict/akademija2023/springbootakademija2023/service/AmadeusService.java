14184da16ae216b3208eaa20028a6a2fd8ff6b78
Ivan Andrić <ivan.andric@king-ict.hr>
2023-06-18 10:02:45 +0200

    Flight search servis za pretraživanje aerodroma

--- src/main/java/hr/kingict/akademija2023/springbootakademija2023/service/AmadeusService.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/service/AmadeusService.java
@@ -5,25 +5,36 @@ import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.referencedata.Locations;
 import com.amadeus.resources.Location;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class AmadeusService {
 
+    Logger logger = LoggerFactory.getLogger(AmadeusService.class);
+
     @Autowired
     private Amadeus amadeus;
+    public List<Location> searchAirports(String keyword) {
 
+        try {
+            Params param =  Params
+                    .with("subType", Locations.AIRPORT)
+                    .and("keyword", keyword);
 
-    public List<Location> getLocation(String keyword) throws ResponseException {
+            return Arrays.asList(amadeus.referenceData.locations
+                    .get(param)
+            );
+        } catch (Exception e) {
 
-        return Arrays.asList(amadeus.referenceData.locations
-                .get(
-                        Params.with("subType", Locations.AIRPORT).and("keyword", keyword))
-        );
-
+            logger.error("Search airports error", e);
+            return Collections.emptyList();
+        }
     }
 }
