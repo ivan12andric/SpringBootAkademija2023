17e077fd90f1c949c631bddf1ad507f20206ec94
Ivan Andrić <ivan.andric@king-ict.hr>
2023-06-19 10:34:34 +0200

    Dodan modul za securiti u pom.
    Implementiran basic security sa userima iz memorije:
    - test/test - pristup test servisima
    - user/user - pristup pretraživanju aerodroma
    - user/user - pristup pretraživanju letova

---
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package hr.kingict.akademija2023.springbootakademija2023.security;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .authorizeHttpRequests(
+                        request -> request
+                                //API-u mogu pristupiti samo korisnici u roli...
+                                .requestMatchers("api/test/**").hasRole("TEST")   //TEST
+                                .requestMatchers("api/flight-search/airports/**").hasRole("USER")  // USER
+                                .requestMatchers("api/flight-search/flights/**").hasRole("ADMIN")  // ADMIN
+                                .requestMatchers(PathRequest.toH2Console()).permitAll() // dozvoljeno za sve korisnike
+                                )
+                .csrf(csrf -> csrf.disable())
+                .headers(header -> header.frameOptions(frameOption -> frameOption.disable()))
+                .httpBasic(httpBasic -> httpBasic.init(httpSecurity))
+                .build();
+
+    }
+
+    @Bean
+    public UserDetailsService getUserDetails() {
+
+        UserDetails testUser = User.withDefaultPasswordEncoder()
+                .username("test")
+                .password("test")
+                .roles("TEST").build();
+
+        UserDetails userUser = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER").build();
+
+        UserDetails adminUser = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(testUser, userUser, adminUser);
+    }
+
+
+}
