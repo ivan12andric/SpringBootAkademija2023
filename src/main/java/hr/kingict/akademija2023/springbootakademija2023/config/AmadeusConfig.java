3e0dce6bc985f49c3ebf9e8b73c197b2a709652e
Ivan Andrić <ivan.andric@king-ict.hr>
2023-06-25 19:54:17 +0200

    Refactoring Amadeus postavki - preseljeno u application.properties

--- src/main/java/hr/kingict/akademija2023/springbootakademija2023/config/AmadeusConfig.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/config/AmadeusConfig.java
@@ -1,11 +1,18 @@
 package hr.kingict.akademija2023.springbootakademija2023.config;
 
 import com.amadeus.Amadeus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmadeusConfig {
+
+    @Value("${amadeus.client-id}")
+    private String clientId;
+
+    @Value("${amadeus.client-secret}")
+    private String clientSecret;
 
     @Bean
     public Amadeus getAmadeus() {
